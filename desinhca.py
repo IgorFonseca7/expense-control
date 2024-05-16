8bbba7a95d58e43264517650d55164dfb10a037b
IgorFonseca7 <igor.galaxy2015@gmail.com>
2024-05-16 12:55:24 -0300

    Add date-time

--- desinhca.py
+++ desinhca.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from datetime import datetime
 
 class App:
     def __init__(self, root):
@@ -17,6 +18,12 @@ class App:
         self.entry_despesa = tk.Entry(self.frame_esquerda, font=('Arial', 14), width=30)
         self.entry_despesa.pack()
 
+        self.label_empresa = tk.Label(self.frame_esquerda, text="Empresa")
+        self.label_empresa.pack()
+
+        self.entry_empresa = tk.Entry(self.frame_esquerda, font=('Arial', 14), width=30)
+        self.entry_empresa.pack()
+
         self.label_valor = tk.Label(self.frame_esquerda, text="Valor")
         self.label_valor.pack()
 
@@ -31,14 +38,17 @@ class App:
 
         self.button = tk.Button(self.frame_esquerda, text="Adicionar", command=self.adicionar_despesa)
         self.button.pack()
+        
 
         self.frame_direita = tk.Frame(root)
         self.frame_direita.grid(row=0, column=1, sticky='n')
 
-        self.tree = ttk.Treeview(self.frame_direita, columns=('Despesa', 'Valor', 'Serviço'), show='headings')
+        self.tree = ttk.Treeview(self.frame_direita, columns=('Despesa', 'Empresa', 'Serviço', 'Valor', 'Data'), show='headings')
         self.tree.heading('Despesa', text='Despesa')
+        self.tree.heading('Empresa', text='Empresa')
         self.tree.heading('Valor', text='Valor')
         self.tree.heading('Serviço', text='Serviço')
+        self.tree.heading('Data', text='Data')
         self.tree.pack()
 
         self.label_total = tk.Label(self.frame_direita, text="Total: R$0.00")
@@ -46,16 +56,19 @@ class App:
 
     def adicionar_despesa(self):
         despesa = self.entry_despesa.get()
+        empresa = self.entry_empresa.get()
         valor = float(self.entry_valor.get())
         servico = self.entry_servico.get()
+        data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")  # Adiciona a data e hora atual
 
-        self.despesas.append((despesa, valor, servico))
-        self.tree.insert('', 'end', values=(despesa, valor, servico))
+        self.despesas.append((despesa, valor, servico, empresa, data))
+        self.tree.insert('', 'end', values=(despesa, valor, servico, empresa, data))
 
-        total = sum(valor for _, valor, _ in self.despesas)
+        total = sum(valor for _, valor, _, _, _ in self.despesas)
         self.label_total.config(text=f"Total: R${total:.2f}")
 
         self.entry_despesa.delete(0, tk.END)
+        self.entry_empresa.delete(0, tk.END)
         self.entry_valor.delete(0, tk.END)
         self.entry_servico.delete(0, tk.END)
 
